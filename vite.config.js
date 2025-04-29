27b714a5011f113d624594575edf16117761c5f5
Ryouta <nextruangkerja@gmail.com>
2025-04-29 19:10:18 +0700

    save

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/special-lazyness/' // Set base to your GitHub repo name with trailing slash for GitHub Pages
+  base: '/kalkulator-gear/'
 })
