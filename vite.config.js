2422720f56baec457ef3a760da15269c22554d7a
Nextorcinus <nextruangkerja@gmail.com>
2025-04-29 15:35:30 +0700

    save

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Set base to root or update to your GitHub repo name with trailing slash
+  base: '/special-lazyness/' // Set base to your GitHub repo name with trailing slash for GitHub Pages
 })
