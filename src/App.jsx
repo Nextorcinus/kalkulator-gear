367f443330edf37c0ba530d41701352901843946
Ryouta <nextruangkerja@gmail.com>
2025-04-29 22:04:33 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,8 @@ function App() {
 }
 
 <footer className="mt-12 text-center text-sm text-gray-500 py-6 border-t">
-&copy; {new Date().getFullYear()} Kalkulator Gear. All rights reserved.
+  &copy; {new Date().getFullYear()} {t('copyright')}
 </footer>
+
 
 export default App;
